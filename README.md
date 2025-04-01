f95e7c7189a01a99d37eaedda5b75c5e9ec25ffc
pengubow <remecedik@gmail.com>
2025-04-01 22:01:49 +0000

    hi

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sound on Silent Mode (iOS)
+This is probably a mod with the smallest amount of lines of code.
+
+<img src="logo.png" width="150" alt="the mod's logo" />
+
+Look at this probably uncentered mod icon 😍
---
+++ src/main.mm
@@ -0,0 +1,19 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <AVFoundation/AVAudioSession.h>
+using namespace geode::prelude;
+
+bool activated = false;
+
+class $modify(MenuLayer) {
+    bool init() {
+
+        if (!MenuLayer::init()) return false;
+        if (!activated) {
+            [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil]; 
+            [[AVAudioSession sharedInstance] setActive:YES error:nil];
+            activated = true;
+        }
+        return true;
+    }
+};
