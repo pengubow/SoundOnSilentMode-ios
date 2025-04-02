33153346b99308600e7164461cbd8be5df5ee96a
pengubow <66965109+pengubow@users.noreply.github.com>
2025-04-02 12:04:08 +0300

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -1,6 +1,5 @@
 #include <Geode/loader/Mod.hpp>
 #include <AVFoundation/AVAudioSession.h>
-
 $on_mod(Loaded) {
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil]; 
     [[AVAudioSession sharedInstance] setActive:YES error:nil];
