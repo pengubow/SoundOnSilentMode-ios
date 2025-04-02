f48391bd2f64ead10c1a429026ba75662bc93a64
pengubow <66965109+pengubow@users.noreply.github.com>
2025-04-02 08:25:14 +0300

    shorten

--- src/main.mm
+++ src/main.mm
@@ -1,18 +1,7 @@
-#include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/loader/Mod.hpp>
 #include <AVFoundation/AVAudioSession.h>
-using namespace geode::prelude;
 
-bool activated = false;
-
-class $modify(MenuLayer) {
-    bool init() {
-        if (!MenuLayer::init()) return false;
-        if (!activated) {
-            [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil]; 
-            [[AVAudioSession sharedInstance] setActive:YES error:nil];
-            activated = true;
-        }
-        return true;
-    }
+$on_mod(Loaded) {
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil]; 
+    [[AVAudioSession sharedInstance] setActive:YES error:nil];
 };
