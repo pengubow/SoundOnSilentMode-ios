bed59b90b72381774260c4edb2a7ace79d5493ed
pengubow <66965109+pengubow@users.noreply.github.com>
2025-04-05 00:43:40 +0300

    v4

--- src/main.mm
+++ src/main.mm
@@ -1,6 +1,27 @@
 #include <Geode/loader/Mod.hpp>
+#include <Geode/loader/SettingV3.hpp>
 #include <AVFoundation/AVAudioSession.h>
+
+using namespace geode::prelude;
+
+$execute {
+    listenForSettingChanges("enable-mod", [](bool enabled) {
+        if (enabled) {
+            [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+        }
+        else {
+            [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategorySoloAmbient error:nil];
+        }
+    });
+}
+
 $on_mod(Loaded) {
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil]; 
+    auto enabled = Mod::get()->getSettingValue<bool>("enable-mod");
+    if (enabled) {
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    }
+    else {
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategorySoloAmbient error:nil];
+    }
     [[AVAudioSession sharedInstance] setActive:YES error:nil];
 };
